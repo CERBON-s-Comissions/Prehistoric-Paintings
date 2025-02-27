06c619616e5afca243f610f5d9bae3c3cf0aa9be
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-02-27 19:54:14 -0300

    Initialize 1.20.4

---
+++ NeoForge/src/main/java/com/cerbon/ml_template/neoforge/ModNameNeo.java
@@ -0,0 +1,13 @@
+package com.cerbon.ml_template.neoforge;
+
+import com.cerbon.ml_template.ModName;
+import com.cerbon.ml_template.util.ModConstants;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ModConstants.MOD_ID)
+public class ModNameNeo {
+
+    public ModNameNeo() {
+        ModName.init();
+    }
+}
---
+++ NeoForge/src/main/java/com/cerbon/ml_template/neoforge/event/NeoEvents.java
@@ -0,0 +1,5 @@
+package com.cerbon.ml_template.neoforge.event;
+
+// Don't Forget to add Event Bus Subscriber | I can't add it without at least one method annotated with @SubscribeEvent
+public class NeoEvents {
+}

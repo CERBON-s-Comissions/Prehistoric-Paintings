671ae478a7cf4b5afa5b1ea291afcb0fed4f0b82
felip <felipecerboncinix@gmail.com>
2025-02-28 18:15:01 -0300

    Add terrestrial landscape 1 and 2

--- Fabric/src/main/java/com/cerbon/ml_template/fabric/ModNameFabric.java
+++ Fabric/src/main/java/com/cerbon/ml_template/fabric/ModNameFabric.java
@@ -3,12 +3,18 @@ package com.cerbon.ml_template.fabric;
 import com.cerbon.ml_template.ModName;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.decoration.PaintingVariant;
 
 public class ModNameFabric implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
         ModName.init();
+
+        Registry.register(BuiltInRegistries.PAINTING_VARIANT, "ml_template:terrestrial_landscape",   new PaintingVariant(64, 64));
+        Registry.register(BuiltInRegistries.PAINTING_VARIANT, "ml_template:terrestrial_landscape_2", new PaintingVariant(64, 64));
     }
 
     @Override
--- NeoForge/src/main/java/com/cerbon/ml_template/neoforge/ModNameNeo.java
+++ NeoForge/src/main/java/com/cerbon/ml_template/neoforge/ModNameNeo.java
@@ -1,13 +1,17 @@
 package com.cerbon.ml_template.neoforge;
 
 import com.cerbon.ml_template.ModName;
+import com.cerbon.ml_template.neoforge.paintings.PPNeoPaintings;
 import com.cerbon.ml_template.util.ModConstants;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(ModConstants.MOD_ID)
 public class ModNameNeo {
 
-    public ModNameNeo() {
+    public ModNameNeo(IEventBus modEventBus) {
         ModName.init();
+
+        PPNeoPaintings.register(modEventBus);
     }
 }
